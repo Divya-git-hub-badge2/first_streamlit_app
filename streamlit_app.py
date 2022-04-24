5874d6e7b064a01bfb504d8883ef79c5e16ad2aa
Divya-git-hub-badge2 <103918205+Divya-git-hub-badge2@users.noreply.github.com>
2022-04-24 15:29:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,12 @@ streamlit.text(fruityvice_response)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered',fruit_choice)  
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
@@ -46,12 +52,6 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered',fruit_choice)  
-
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 
