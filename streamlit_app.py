717868e3548071d103bb41d145e3d71b3cd93a4a
Divya-git-hub-badge2 <103918205+Divya-git-hub-badge2@users.noreply.github.com>
2022-04-24 15:21:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 streamlit.text(fruityvice_response)
 
 
-#New Section to display fruityvice api response
-sreamlit.header('Fruityvice Fruit Advice!')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
 
 
